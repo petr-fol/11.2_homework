40ad909ebce405a77af3bdf210043984a54c825b
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-09-21 00:51:30 +0600

    update str_func.py, correct main.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,34 @@
-def str_func(word):
+def str_upper_word(word):
     """
     :param word:
     :return строка в верхнем регистре:
     """
     return word.upper()
+
+
+def is_letter(symbol):
+    if symbol not in "'\"!@#$%^&*()_+=-.,;:?/0123456789":
+        return True
+    else:
+        return False
+
+
+def str_upper_letter(str_):
+    """
+    :param str_:
+    :return: строку, в которой у каждого слова первая буква заглавная
+    """
+    word_list = str_.split(' ')
+    new_list_words = []
+
+    for word in word_list:
+        new_word = ''
+        for i, char in enumerate(word):
+            if i == 0 and is_letter(char):
+                new_word += char.upper()
+            else:
+                new_word += char
+        new_list_words.append(new_word)
+
+    new_str_words = ' '.join(new_list_words)
+    return new_str_words
